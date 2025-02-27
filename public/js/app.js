cbce2a82f792e03daba2c896ffc6d0828bc5fa71
Kaartik Arora <kaartikarora0001@gmail.com>
2025-02-27 18:24:09 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const weatherForm = document.querySelector('form');
+const searchElement=document.querySelector('input');
+const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit',(event)=>{
+    event.preventDefault() // form ka natural behaviour to reload the page upon submission  and reset all the value and we dont want that so we remove that 
+    //prevent form from reloading the page 
+     
+    const location=searchElement.value
+    messageOne.textContent='Loading Result...'
+    messageTwo.textContent=''
+    fetch('http://localhost:3000/weather?address='+ encodeURIComponent(location)).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                messageOne.textContent='ERROR...'
+                messageTwo.textContent=data.error
+            }
+            else{
+                messageOne.textContent='Fetched results for the location : ' + data.location   //manipulate the value of the para from js file 
+                messageTwo.textContent='Forecast : '+ data.forecast.temperature + data.forecast.type + data.forecast.rain
+            }
+        })
+    })
+})
+
+
+//error to be fixed : invalid values 
